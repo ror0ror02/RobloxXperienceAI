728df7add59473126f8f7913a3633a05084bd891
ror0ror02 <142999522+ror0ror02@users.noreply.github.com>
2023-12-07 19:14:10 +0700

    Update InRXAIWindows.bat

--- InRXAIWindows.bat
+++ InRXAIWindows.bat
@@ -1,22 +1,51 @@
+
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-rem Проверка наличия Git
-where git >nul 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo Ошибка: Git не найден. Установите Git и повторите попытку.
-    exit /b 1
+:: Проверка наличия Git и Python
+echo Checking for Git and Python...
+where git >nul 2>nul
+IF !ERRORLEVEL! NEQ 0 (
+    echo Git is not installed. Please install Git and try again.
+    exit /b
+)
+where python >nul 2>nul
+IF !ERRORLEVEL! NEQ 0 (
+    echo Python is not installed. Please install Python and try again.
+    exit /b
+)
+echo Git and Python found.
+
+:: Клонирование или обновление репозитория
+echo Checking RobloxXperienceAI repository...
+if not exist "RobloxXperienceAI" (
+    echo Cloning RobloxXperienceAI repository...
+    git clone https://github.com/ror0ror02/RobloxXperienceAI.git
+) else (
+    echo RobloxXperienceAI repository already exists. Pulling latest updates...
+    cd RobloxXperienceAI
+    git pull
+    cd..
 )
 
-
-rem Проверка наличия Python
-where python >nul 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo Ошибка: Python не найден. Установите Python и повторите попытку.
-    exit /b 1
-)
-
-
-rem Установка зависимостей
+:: Установка зависимостей
+cd RobloxXperienceAI
+echo Installing dependencies...
 pip install -r r_requirements.txt
+IF !ERRORLEVEL! NEQ 0 (
+    echo Error occurred during installation of dependencies.
+    exit /b
+)
+echo Dependencies installed successfully.
 
-echo Установка завершена. Теперь вы можете запустить проект, следуя инструкциям в README.md.
+:: Запуск RobloxXperienceAI
+echo Launching RobloxXperienceAI...
+cd Networks RXAI folder
+python RXAI-New.py
+IF !ERRORLEVEL! NEQ 0 (
+    echo Error occurred during launching RobloxXperienceAI.
+    exit /b
+)
+
+echo RobloxXperienceAI launched successfully.
+ENDLOCAL
